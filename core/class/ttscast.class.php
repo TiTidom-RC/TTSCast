ea5638f42596019fd772d6110979c7ce09ec8c47
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-01-07 19:21:40 +0100

    Update ttscast.class.php

--- core/class/ttscast.class.php
+++ core/class/ttscast.class.php
@@ -401,8 +401,8 @@ class ttscastCmd extends cmd
         $eqLogic = $this->getEqLogic();
         $logicalId = $this->getLogicalId();
         
-        log::add('ttscast', 'debug', '[CMD] eqLogic :: ' . str($eqLogic));
-        log::add('ttscast', 'debug', '[CMD] LogicalId :: ' . str($logicalId));
+        log::add('ttscast', 'debug', '[CMD] eqLogic :: ');
+        log::add('ttscast', 'debug', '[CMD] LogicalId :: ');
 
         if ( $this->GetType = "action" ) {
 			// $eqLogic->getCmd();
