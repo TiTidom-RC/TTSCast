f2418da7697ca9589f93b07f72c3f3a42e5c4d78
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-01-17 22:20:55 +0100

    Update ttscastd.py

--- resources/ttscastd/ttscastd.py
+++ resources/ttscastd/ttscastd.py
@@ -210,7 +210,7 @@ def scanChromeCast(_mode='UNKOWN'):
             currentTime = int(time.time())
             currentTimeStr = datetime.datetime.fromtimestamp(currentTime).strftime("%d/%m/%Y - %H:%M:%S")
 
-            chromecasts, browser = pychromecast.get_listed_chromecasts(uuids=Config.KNOWN_DEVICES, known_hosts=Config.KNOWN_DEVICES)
+            chromecasts, browser = pychromecast.get_listed_chromecasts(known_hosts=Config.KNOWN_DEVICES)
             
             logging.debug('[DAMEON][SCANNER][SCHEDULE] Nb Cast :: %s', len(chromecasts))
             for cast in chromecasts: 
