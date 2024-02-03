8eb53d4b38837fd937545a15a48bc1b548532fb2
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-02-03 09:06:34 +0100

    Update utils.py

--- resources/ttscastd/utils.py
+++ resources/ttscastd/utils.py
@@ -39,7 +39,7 @@ class Config:
     ScanSchedule = 60
     ScanLastTime = int(time.time())
     
-    HeartbeatFrequency = 300
+    HeartbeatFrequency = 60
     HeartbeatLastTime = int(time.time())
     
     ResourcesLastUsed = 0
