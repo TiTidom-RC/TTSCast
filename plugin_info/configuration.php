d25afe6592fc1f132b205b95632c7b1cd315be95
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-02-10 19:17:59 +0100

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -37,7 +37,7 @@ if (!isConnect()) {
             <legend><i class="fas fa-university"></i> {{Démon}}</legend>
             <div class="form-group">
                 <label class="col-lg-3 control-label">{{Port Socket Interne}}
-                    <sup><i class="fas fa-exclamation-triangle tooltips" style="color:var(--al-danger-color)!important;" title="{{Le démon devra être redémarré après la modification de ce paramètre}}"></i></sup>    
+                    <sup><i class="fas fa-exclamation-triangle tooltips" style="color:var(--al-warning-color)!important;" title="{{Le démon devra être redémarré après la modification de ce paramètre}}"></i></sup>    
                     <sup><i class="fas fa-question-circle tooltips" title="{{[ATTENTION] Ne changez ce paramètre qu'en cas de nécessité. (Défaut = 55111)}}"></i></sup>
                 </label>
                 <div class="col-lg-1">
@@ -46,7 +46,7 @@ if (!isConnect()) {
             </div>
             <div class="form-group">
 	            <label class="col-lg-3 control-label">{{Fréquence des cycles}}
-                    <sup><i class="fas fa-exclamation-triangle tooltips" style="color:var(--al-danger-color)!important;" title="{{Le démon devra être redémarré après la modification de ce paramètre}}"></i></sup>    
+                    <sup><i class="fas fa-exclamation-triangle tooltips" style="color:var(--al-warning-color)!important;" title="{{Le démon devra être redémarré après la modification de ce paramètre}}"></i></sup>    
                     <sup><i class="fas fa-question-circle tooltips" title="{{Facteur multiplicateur des cycles du démon (Défaut = 1)}}"></i></sup>
                 </label>
 	            <div class="col-lg-2">
@@ -89,7 +89,7 @@ if (!isConnect()) {
             </div>
             <div class="form-group customform-voicersstts">
                 <label class="col-lg-3 control-label">{{Clé API (Voice RSS)}} <a class="btn btn-info btn-xs" target="_blank" href="https://www.voicerss.org/personel/">{{SITE}}</a>
-                    <sup><i class="fas fa-exclamation-triangle tooltips" style="color:var(--al-danger-color)!important;" title="{{Le démon devra être redémarré après la modification de ce paramètre}}"></i></sup>    
+                    <sup><i class="fas fa-exclamation-triangle tooltips" style="color:var(--al-warning-color)!important;" title="{{Le démon devra être redémarré après la modification de ce paramètre}}"></i></sup>    
                     <sup><i class="fas fa-question-circle tooltips" title="{{Rentrer votre clé API Voice RSS, récupérable sur leur site, dans votre profil'}}"></i></sup>
                 </label>
                 <div class="col-lg-3">
@@ -98,7 +98,7 @@ if (!isConnect()) {
             </div>
             <div class="form-group customform-gcloudtts">
                 <label class="col-lg-3 control-label">{{Clé API (gCloud TTS)}}
-                    <sup><i class="fas fa-exclamation-triangle tooltips" style="color:var(--al-danger-color)!important;" title="{{Le démon devra être redémarré après la modification de ce paramètre}}"></i></sup>    
+                    <sup><i class="fas fa-exclamation-triangle tooltips" style="color:var(--al-warning-color)!important;" title="{{Le démon devra être redémarré après la modification de ce paramètre}}"></i></sup>    
                     <sup><i class="fas fa-question-circle tooltips" title="{{Uploader votre clé JSON en utilisant le bouton 'Ajouter Clé (JSON)'}}"></i></sup>
                 </label>
                 <div class="col-lg-3">
@@ -276,7 +276,7 @@ if (!isConnect()) {
             <legend><i class="fas fa-list-alt"></i> {{Options}}</legend>
             <div class="form-group">
                 <label class="col-lg-3 control-label">{{URL Jeedom Externe}}
-                    <sup><i class="fas fa-exclamation-triangle tooltips" style="color:var(--al-danger-color)!important;" title="{{Le démon devra être redémarré après la modification de ce paramètre}}"></i></sup>    
+                    <sup><i class="fas fa-exclamation-triangle tooltips" style="color:var(--al-warning-color)!important;" title="{{Le démon devra être redémarré après la modification de ce paramètre}}"></i></sup>    
                     <sup><i class="fas fa-question-circle tooltips" title="{{Utilise l'URL externe de Jeedom pour la lecture des fichiers TTS plutôt que l'URL interne (Recommandé = décoché)}}"></i></sup>
                 </label>
                 <div class="col-lg-2">
