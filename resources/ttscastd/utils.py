10f142c3f117a9f8ce76632f3c7a31ffba9ec8ec
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2025-10-05 17:58:32 +0200

    Update utils.py

--- resources/ttscastd/utils.py
+++ resources/ttscastd/utils.py
@@ -76,7 +76,7 @@ class Config:
     
     def aiSysPrompt(self, aiCustomTone=None):
         _aiTone = aiCustomTone if aiCustomTone else self.aiDefaultTone
-        return 'Répond à la question posée seulement s\'il y en a une, sinon reformule simplement la phrase, sur un ton ' + _aiTone + '. Sois clair, bref et concis et conserve les valeurs chiffrées s\'il y en a. Ne pas utiliser d\'emojis. Ne commente pas tes propres actions ou le processus de génération.'
+        return 'Répond à la question posée seulement s\'il y en a une, sinon reformule simplement la phrase, sur un ton ' + _aiTone + '. Utilise la recherche en ligne pour déterminer la date précise du jour. Sois clair, bref et concis et conserve les valeurs chiffrées s\'il y en a. Ne pas utiliser d\'emojis. Ne commente pas tes propres actions ou le processus de génération.'
 
     aiUseCustomSysPrompt = False
     aiCustomSysPrompt = ''
