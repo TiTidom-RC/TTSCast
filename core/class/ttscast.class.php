9c9a896214af7bf73572b431db0f4b71a0db6c10
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2025-12-13 21:16:12 +0100

    Update ttscast.class.php

--- core/class/ttscast.class.php
+++ core/class/ttscast.class.php
@@ -1949,7 +1949,6 @@ class ttscastCmd extends cmd
         $eqLogic = $this->getEqLogic();
         // Empêcher la suppression automatique des commandes de l'équipement virtuel AI Stats
         if (is_object($eqLogic) && $eqLogic->getLogicalId() == 'TTSCast_AI_Stats') {
-            message::add(__('Les commandes de l\'équipement TTSCast AI Stats ne peuvent pas être supprimées. Désactivez la fonction IA dans la configuration du plugin pour supprimer l\'équipement complet.', __FILE__), 'warning');
             return true;
         }
         return false;
@@ -1957,6 +1956,10 @@ class ttscastCmd extends cmd
 
     // Fonction exécutée automatiquement avant la suppression de la commande
     public function preRemove() {
+        $eqLogic = $this->getEqLogic();
+        if (is_object($eqLogic) && $eqLogic->getLogicalId() == 'TTSCast_AI_Stats') {
+            throw new Exception(__('Les commandes de l\'équipement TTSCast AI Stats ne peuvent pas être supprimées. Désactivez la fonction IA dans la configuration du plugin pour supprimer l\'équipement complet.', __FILE__));
+        }
     }
 
     // Exécution d'une commande
